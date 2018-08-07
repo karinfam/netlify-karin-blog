0cf6e35baeae295ce83e5d78e146903520755a1f
Karin Fam <karinfam@outlook.com>
2018-08-07 23:47:04 +0800

    - Changed header image

--- src/components/HeaderImage/Wrapper.js
+++ src/components/HeaderImage/Wrapper.js
@@ -1,12 +1,12 @@
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
-  border: 5px solid #3598db;
+  border: 5px solid #ffffff;
   border-radius: 50%;
   height: 150px;
   margin: 0 auto 15px auto;
   overflow: hidden;
-  width: 150px;
+  width: 140px;
 
   img {
     padding: 5px;
